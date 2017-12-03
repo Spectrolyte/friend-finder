8999f3789ecc1f1a68b37d4ce24f6a85c924ff56
Diana <dianaltecson@gmail.com>
2017-12-02 18:12:57 -0800

    added comments; routed html pages to server

---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+// A GET Route to /survey which should display the survey page.
+// A default, catch-all route that leads to home.html which displays the home page.
+var express = require('express')
+var router = express.Router();
+var path = require('path');
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+});
+
+router.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+})
+
+module.exports = router;
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use(htmlRoutes);
 
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
