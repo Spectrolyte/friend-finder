2ef99e48ca58a2cea8b17c2d317ca35af07d4e07
Diana <dianaltecson@gmail.com>
2017-12-04 08:58:52 -0800

    calculated differences and made a list for later comparison

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -3,6 +3,11 @@ var friends = [
         name: 'Bob',
         photo: 'https://timedotcom.files.wordpress.com/2014/08/t100_tv_spongebob_free1.jpg?quality=85&w=1024',
         scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: 'Pat',
+        photo: 'https://vignette.wikia.nocookie.net/spongebob/images/3/33/Patrick_Star.svg/revision/latest/scale-to-width-down/270?cb=20100724183918',
+        scores: [2,2,2,2,2,2,2,2,2,2]
     }
 ];
 
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -17,9 +17,52 @@ router.post('/api/friends', function (req, res) {
     for (var i = 0; i < newFriend.scores.length; i++) {
         newFriend.scores[i] = parseInt(newFriend.scores[i]);
     }
+    // find best match
+    calculateDiff([4,2,1,1,1,1,1,1,1,1], friends);
+    findMatch([34,56,23,80]);
     // add new data to friends list
     friends.push(newFriend);
     res.end();
 })
+
+function calculateDiff (userScores, friendsData) {
+    // takes in two arrays -- current user's scores and the current friends list
+    var scoreList = [];
+    // iterate through friends list, capture their scores and put into an array
+    for (var i=0; i < friendsData.length; i ++) {
+        scoreList.push(friendsData[i].scores);
+    }
+
+    // difference scores stores here
+    var differences = [];
+
+    for (var i=0; i < scoreList.length; i++) {
+        // current friend being compared
+        var friendScores = scoreList[i];
+        // total diff is the amount of differences accumulated between current friend and current user
+        var totalDiff = 0;
+        // calculate difference between each question -- nested
+        for (var j=0; j < userScores.length; j++) {
+            var diff = userScores[j] - friendScores[j];
+            // accounts for negative differences
+            diff = Math.abs(diff);
+            totalDiff += diff;
+        }
+        differences.push(totalDiff);
+    }
+    
+
+    console.log('diffs: ' + differences);
+}
+
+// finds friend with most compatibility -- least difference in score
+function findMatch (differences) {
+    // takes in an array populated with the differences between the current user and potential matches
+    // find the least value and return that person's info
+    differences.sort(function compareNumbers(a, b) {
+        return a - b;
+    })
+    
+}
 
 module.exports = router;
